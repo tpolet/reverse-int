250b81ad82f7e0ff9f7d7cc77437061462e40b0d
Tatiana Poletaeva <tpolet@tatianas-mbp.home>
2024-08-18 09:15:04 +0100

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ function reverse(num){
     for (let i = 1; i < num.toString().length+1; i++) {
         str = str + num.toString().charAt(num.toString().length-i) ;
       };
-    return str;
+    return Number(str);
 }
-
-const result=reverse(123);
-console.log(result);
